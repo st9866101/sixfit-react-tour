074e93809c5a7ec842135df7f34051c63d370397
st9866101 <st9866101@gmail.com>
2022-08-23 15:23:38 +0800

    Updates

---
+++ precache-manifest.1c8d973b20af0d9e17ace2c61e27c106.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a3771415960b215c784",
+    "url": "/sixfit-react-tour/static/css/main.400b390c.chunk.css"
+  },
+  {
+    "revision": "9a3771415960b215c784",
+    "url": "/sixfit-react-tour/static/js/main.9a377141.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/sixfit-react-tour/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "3d4f0c933a8d79f49449",
+    "url": "/sixfit-react-tour/static/css/2.225ed0a2.chunk.css"
+  },
+  {
+    "revision": "3d4f0c933a8d79f49449",
+    "url": "/sixfit-react-tour/static/js/2.3d4f0c93.chunk.js"
+  },
+  {
+    "revision": "05a9282bf3c8ac654649",
+    "url": "/sixfit-react-tour/static/js/runtime~main.05a9282b.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/sixfit-react-tour/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9e40e19e8a839670c8c7a009d5f86448",
+    "url": "/sixfit-react-tour/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sixfit-react-tour/precache-manifest.7ad75438488166ad95a5f0973aaf9b9b.js"
+  "/sixfit-react-tour/precache-manifest.1c8d973b20af0d9e17ace2c61e27c106.js"
 );
 
 workbox.clientsClaim();
